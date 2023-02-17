ad38a16c52ad93423000a513f1fd439501e509b9
Rapp Anna <anjarapp18@gmail.com>
2023-02-17 16:21:35 +0700

    Добавила форму для регистрации Верстка формы для регистрации, обработчик нажатия для ее открытия

--- authorize/script.js
+++ authorize/script.js
@@ -1,10 +1,19 @@
 
 $('.switch input').change(function (e) { 
     e.preventDefault();
+    $('.register-form').addClass('d-none');
  	if($(this).prop('checked')){
         $('.can-registration').addClass('d-none')
-    }
-    else {
+        $('.login-form').removeClass('d-none')
+        
+    } else {
         $('.can-registration').removeClass('d-none')
     }
+});
+
+$('#btn-registration').click(function (e) { 
+    e.preventDefault();
+    $('.login-form').addClass('d-none');
+    $('.can-registration').addClass('d-none');
+    $('.register-form').removeClass('d-none');
 });
