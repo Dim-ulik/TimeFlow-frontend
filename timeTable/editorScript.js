e32a553ba2ceb1c8276e40967f40ff7d630adb6f
Dim-ulik <d.martyshev@yandex.ru>
2023-02-22 22:06:16 +0700

    code style

--- timeTable/editorScript.js
+++ timeTable/editorScript.js
@@ -1,17 +1,52 @@
+const createFreeCell = (day, pairNumber) => {
+    let addPairCell = $("#add-pair-template").clone();
+    let cellId = day + "." + pairNumber;
+
+    addPairCell.attr("id", cellId);
+    setText(addPairCell.find(".pair-time"), getPairTime(pairNumber));
+    addPairCell.removeClass("d-none");
+
+    return addPairCell;
+}
+
 const createFreeTable = () => {
     for (let day = 1; day <= 6; day++) {
         for (let pairNumber = 1; pairNumber <= pairsOnDayAmount; pairNumber++) {
-            let addPairCell = $("#add-pair-template").clone();
-            let cellId = day + "." + pairNumber;
-            addPairCell.attr("id", cellId);
-            addPairCell.removeClass("d-none");
-            $(chooseDay(day)).append(addPairCell);
+            createFreeCell(day, pairNumber);
         }
     }
 }
 
-createFreeTable();
-
-const createTimetableToRedact = () => {
-
+const clearTimetable = () => {
+    for (let day = 1; day <= 6; day++) {
+        for (let pairNumber = 1; pairNumber <= pairsOnDayAmount; pairNumber++) {
+                $(chooseDay(day)).children().last().remove();
+            }
+        }
 }
+
+const createTimetableToRedact = (responseTimetable) => {
+    let currentPairs = createTimetableMatrix(responseTimetable);
+    for (let i = 0; i < currentPairs.length; i++) {
+        if (currentPairs[i].length === 0) {
+            for (let j = 0; j < pairsOnDayAmount; j++) {
+                let currentCell = createFreeCell(i+1, j+1);
+                addPair(currentCell, i+1)
+            }
+        }
+        else {
+            for (let j = 0; j < pairsOnDayAmount; j++) {
+                if (currentPairs[i][j] === undefined || currentPairs[i][j].pairType === 0) {
+                    let currentCell = createFreeCell(i+1, j+1);
+                    addPair(currentCell, i+1)
+                }
+                else {
+                    let currentPair = createPair(currentPairs[i][j].pairType, currentPairs[i][j].pairNumber, currentPairs[i][j].pairDay, currentPairs[i][j].pairName, currentPairs[i][j].pairRoom, currentPairs[i][j].teacherName,"#pair-template-with-edit");
+                    addPair(currentPair, currentPairs[i][j].pairDay)
+                }
+            }
+        }
+    }
+}
+
+createTimetableToRedact(createTimetableMatrix(timeTable972101));
--- timeTable/timeTableScript.js
+++ timeTable/timeTableScript.js
@@ -1,88 +1,7 @@
-let pairsOnDayAmount = 0;
-let timeslotsList = [];
-
-let response = [
-    {sequenceNumber: 1, beginTime: "8:45", endTime: "10:20"},
-    {sequenceNumber: 2, beginTime: "10:35", endTime: "12:10"},
-    {sequenceNumber: 3, beginTime: "12:25", endTime: "14:00"},
-    {sequenceNumber: 4, beginTime: "14:45", endTime: "16:20"},
-    {sequenceNumber: 5, beginTime: "16:35", endTime: "18:10"},
-    {sequenceNumber: 6, beginTime: "18:25", endTime: "20:00"},
-]
-
-const createTimeslotsList = (response, timeslotsList) => {
-    pairsOnDayAmount = response.length;
-
-    for (let i = 0; i < pairsOnDayAmount; i++) {
-        timeslotsList[i] = response[i].beginTime + " - " + response[i].endTime;
-    }
-}
-
 const addPair = (pair, day) => {
     let dayId = chooseDay(day);
     $(dayId).append(pair);
 }
-
-const getPairTypeInf = (pairType) => {
-    let pairTypeInf = {
-        pairClass: "",
-        typeString: ""
-    }
-
-    switch (pairType) {
-        case "LECTURE":
-            pairTypeInf.pairClass = "lecture-pair";
-            pairTypeInf.typeString = "Лекция";
-            break;
-        case "SEMINAR":
-            pairTypeInf.pairClass = "seminar-pair";
-            pairTypeInf.typeString = "Семинар";
-            break;
-        case "PRACTICAL-LESSON":
-            pairTypeInf.pairClass = "practice-pair";
-            pairTypeInf.typeString = "Практика";
-            break;
-        case "LABORATORY-LESSON":
-            pairTypeInf.pairClass = "laboratory-pair";
-            pairTypeInf.typeString = "Лабораторная";
-            break;
-        case "EXAM":
-            pairTypeInf.pairClass = "exam-pair";
-            pairTypeInf.typeString = "Экзамен";
-            break;
-    }
-
-    return pairTypeInf;
-}
-
-const createPair = (pairType, pairPlace, pairTime, pairName, pairRoom, teacherName) => {
-    let pair;
-    let pairTypeString = getPairTypeInf(pairType).typeString;
-    let pairTypeClass = getPairTypeInf(pairType).pairClass;
-
-    if (pairType !== 0) {
-        pair = $("#pair-template").clone();
-        pair.addClass(pairTypeClass);
-    }
-    else {
-        pair = $("#no-pair-template").clone();
-    }
-
-    setText(pair.find(".pair-time"), pairTime);
-    let pairId = pairPlace.day.toString() + "." + pairPlace.number.toString();
-    pair.attr("id", pairId);
-
-    if (pairType !== 0) {
-        setText(pair.find(".pair-type"), pairTypeString);
-        setText(pair.find(".pair-name"), pairName);
-        setText(pair.find(".pair-room"), pairRoom);
-        setText(pair.find(".teacher-name"), teacherName);
-    }
-
-    pair.removeClass("d-none");
-    addPair(pair, pairPlace.day);
-}
-
 
 const createFreeDay = (day) => {
     let cell = $("#free-day-template").clone();
@@ -90,18 +9,17 @@ const createFreeDay = (day) => {
     addPair(cell, day);
 }
 
-const createTimetable = (array) => {
+const showTimetable = (matrix) => {
     for (let i = 0; i < 6; i++) {
-        if (array[i].length !== 0) {
-            for (let j = 0; j < array[i].length; j++) {
-                let pairPlace = {
-                    day: i+1, number: j+1
-                }
-                if (array[i][j].pairType === 0) {
-                    createPair(0, pairPlace, array[i][j].pairTime);
+        if (matrix[i].length !== 0) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j].pairType === 0) {
+                    let currentPair = createPair(0, matrix[i][j].pairNumber, matrix[i][j].pairDay, matrix[i][j].pairTime);
+                    addPair(currentPair, matrix[i][j].pairDay);
                 }
                 else {
-                    createPair(array[i][j].pairType, pairPlace, array[i][j].pairTime, array[i][j].pairName, array[i][j].pairRoom, array[i][j].teacherName);
+                    let currentPair = createPair(matrix[i][j].pairType, matrix[i][j].pairNumber, matrix[i][j].pairDay, matrix[i][j].pairName, matrix[i][j].pairRoom, matrix[i][j].teacherName, "#pair-template");
+                    addPair(currentPair, matrix[i][j].pairDay);
                 }
             }
         }
@@ -111,46 +29,4 @@ const createTimetable = (array) => {
     }
 }
 
-let timeTable972101 = [
-    [
-        {pairType: "LECTURE", pairTime: "8:45 - 10:20", pairName: "Основы машинного обучения", pairRoom: "Ауд. 328 (2)", teacherName: "Красавин Дмитрий Сергеевич"},
-        {pairType: 0, pairTime: "8:45 - 10:20"},
-        {pairType: "PRACTICAL-LESSON", pairTime: "10:35 - 12:10", pairName: "Основы машинного обучения", pairRoom: "Ауд. 214 (2)", teacherName: "Красавин Дмитрий Сергеевич"}
-    ],
-    [],
-    [
-        {pairType: "EXAM", pairTime: "8:45 - 10:20", pairName: "Тестирование программного обеспечения", pairRoom: "Ауд. 216 (2)", teacherName: "Волков Максим Николаевич"},
-        {pairType: "PRACTICAL-LESSON", pairTime: "12:25 - 14:00", pairName: "ООП", pairRoom: "Ауд. 101 (2)", teacherName: "Змеев Денис Олегович"},
-        {pairType: 0, pairTime: "14:45 - 16:20"},
-        {pairType: "LABORATORY-LESSON", pairTime: "16:35 - 18:10", pairName: "ООП", pairRoom: "Ауд. 202 (2)", teacherName: "Змеев Денис Олегович"},
-    ],
-    [],
-    [
-        {pairType: "LABORATORY-LESSON", pairTime: "8:45 - 10:20", pairName: "Математика для компьютерных наук ч.3", pairRoom: "Ауд. 216 (2)", teacherName: "Даммер Диана Дамировна"},
-    ],
-    []
-]
-
-let timeTable972102 = [
-    [],
-    [
-        {pairType: "SEMINAR", pairTime: "8:45 - 10:20", pairName: "Основы машинного обучения", pairRoom: "Ауд. 328 (2)", teacherName: "Красавин Дмитрий Сергеевич"},
-        {pairType: 0, pairTime: "8:45 - 10:20"},
-        {pairType: "PRACTICAL-LESSON", pairTime: "10:35 - 12:10", pairName: "Основы машинного обучения", pairRoom: "Ауд. 214 (2)", teacherName: "Красавин Дмитрий Сергеевич"}
-    ],
-    [],
-    [
-        {pairType: "LABORATORY-LESSON", pairTime: "8:45 - 10:20", pairName: "Математика для компьютерных наук ч.3", pairRoom: "Ауд. 216 (2)", teacherName: "Даммер Диана Дамировна"},
-    ],
-    [],
-    [
-        {pairType: "EXAM", pairTime: "8:45 - 10:20", pairName: "Тестирование программного обеспечения", pairRoom: "Ауд. 216 (2)", teacherName: "Волков Максим Николаевич"},
-        {pairType: "PRACTICAL-LESSON", pairTime: "12:25 - 14:00", pairName: "ООП", pairRoom: "Ауд. 101 (2)", teacherName: "Змеев Денис Олегович"},
-        {pairType: 0, pairTime: "14:45 - 16:20"},
-        {pairType: "LABORATORY-LESSON", pairTime: "16:35 - 18:10", pairName: "ООП", pairRoom: "Ауд. 202 (2)", teacherName: "Змеев Денис Олегович"},
-    ]
-]
-
-createTimeslotsList(response);
-
-console.log(timeslotsList);
+showTimetable(createTimetableMatrix(timeTable972101));
