e9bffa25ee6b558349f6870ce920c993816b2738
Rapp Anna <anjarapp18@gmail.com>
2023-02-22 19:40:16 +0700

    Избавилась от отдельного входа для админа Удалила слайдер на странице входа и регистрации Поправила страницу с учетом изменений

--- authorize/script.js
+++ authorize/script.js
@@ -1,19 +1,12 @@
 
-$('.switch input').change(function (e) { 
-    e.preventDefault();
-    $('.register-form').addClass('d-none');
- 	if($(this).prop('checked')){
-        $('.can-registration').addClass('d-none')
-        $('.login-form').removeClass('d-none')
-        
-    } else {
-        $('.can-registration').removeClass('d-none')
-    }
-});
-
 $('#btn-registration').click(function (e) { 
     e.preventDefault();
     $('.login-form').addClass('d-none');
-    $('.can-registration').addClass('d-none');
     $('.register-form').removeClass('d-none');
 });
+
+$('#go_to_login').click(function (e) {
+    e.preventDefault();
+    $('.login-form').removeClass('d-none');
+    $('.register-form').addClass('d-none');
+})
