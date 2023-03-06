3baf11fc07fdcd70a34e08bc89a7b53b3fff03dd
Rapp Anna <anjarapp18@gmail.com>
2023-03-06 23:09:32 +0700

    load teachers in teachers list

--- adminPanel/adminPanel.js
+++ adminPanel/adminPanel.js
@@ -5,42 +5,63 @@ $(document).ready(function () {
   let activePage = "users_list";
   changeNavbar(activePage);
   changeFiltration(activePage);
+  loadTeachers();
 });
 
+function loadTeachers() {
+  fetch("http://94.103.87.164:8081/api/v1/teachers")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      let template = $("#teacher-template");
+
+      for (let teacher of json) {
+        let block = template.clone();
+        block.removeClass("d-none");
+        block.attr("value", teacher.id);
+        block.text(
+          teacher.surname + " " + teacher.name + " " + teacher.patronymic
+        );
+        $(".teachers-list").append(block);
+      }
+    });
+}
+
 $(".navbar-active").click(function (e) {
-  e.preventDefault()
-  changeNavbar($(this).attr("id"))
-  changeFiltration($(this).attr("value"))
-  changeContent($(this).attr("id"))
+  e.preventDefault();
+  changeNavbar($(this).attr("id"));
+  changeFiltration($(this).attr("value"));
+  changeContent($(this).attr("id"));
 });
 
 function changeContent(activePage) {
-    switch (activePage) {
-        case "users_list":
-            $('.header').text('Список пользователей');
-            $('.header').attr('value', 'users');
-          break;
-        case "app_employee":
-            $('.header').text('Заявки сотрудников');
-            $('.header').attr('value', 'employee');
-            loadApllications($('.header').attr('value'), 0, pageSize);
+  switch (activePage) {
+    case "users_list":
+      $(".header").text("Список пользователей");
+      $(".header").attr("value", "users");
+      break;
+    case "app_employee":
+      $(".header").text("Заявки сотрудников");
+      $(".header").attr("value", "employee");
+      loadApllications($(".header").attr("value"), 0, pageSize);
 
-          break;
-        case "app_schedule-maker":
-            $('.header').text('Заявки составителей');
-            $('.header').attr('value', 'schedule-maker');
-            loadApllications($('.header').attr('value'), 0, pageSize);
+      break;
+    case "app_schedule-maker":
+      $(".header").text("Заявки составителей");
+      $(".header").attr("value", "schedule-maker");
+      loadApllications($(".header").attr("value"), 0, pageSize);
 
-          break;
-        case "app_student":
-            $('.header').text('Заявки студентов');
-            $('.header').attr('value', 'student');
-            loadApllications($('.header').attr('value'), 0, pageSize);
+      break;
+    case "app_student":
+      $(".header").text("Заявки студентов");
+      $(".header").attr("value", "student");
+      loadApllications($(".header").attr("value"), 0, pageSize);
 
-          break;
-        default:
-          break;
-      }
+      break;
+    default:
+      break;
+  }
 }
 
 function changeNavbar(activePage) {
@@ -56,15 +77,27 @@ function changeFiltration(activePage) {
 $(".apply-applications-filters").click(function (e) {
   e.preventDefault();
   let formData = new FormData(app_filters);
-  let typeOfUser = $('.header').attr('value');
+  let typeOfUser = $(".header").attr("value");
   let pageNumber = 0;
-  loadApllications(typeOfUser, pageNumber, pageSize, formData.get('sortDirection'), formData.get('isClosed'))
+  loadApllications(
+    typeOfUser,
+    pageNumber,
+    pageSize,
+    formData.get("sortDirection"),
+    formData.get("isClosed")
+  );
 });
 
 $(".apply-users-list-filters").click(function (e) {
-    e.preventDefault();
-    let formData = new FormData(app_filters);
-    let typeOfUser = $('.header').attr('value');
-    let pageNumber = 0;
-    loadApllications(typeOfUser, pageNumber, pageSize, formData.get('sortDirection'), formData.get('isClosed'))
-  });
+  e.preventDefault();
+  let formData = new FormData(app_filters);
+  let typeOfUser = $(".header").attr("value");
+  let pageNumber = 0;
+  loadApllications(
+    typeOfUser,
+    pageNumber,
+    pageSize,
+    formData.get("sortDirection"),
+    formData.get("isClosed")
+  );
+});
