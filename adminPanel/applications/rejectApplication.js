847c680f6e8307b18edd3e180731fb7af8c3a6b1
Rapp Anna <anjarapp18@gmail.com>
2023-03-08 23:22:51 +0700

    fix reject application

--- adminPanel/applications/rejectApplication.js
+++ adminPanel/applications/rejectApplication.js
@@ -1,23 +1,15 @@
-import deleteApplication from "./deleteApplication.js";
-
-function rejectApplication(id) {
-    fetch(`http://94.103.87.164:8081/api/v1/request/employee/schedule-maker/${id}/reject`, {
+function rejectApplication(typeOfUser, id) {
+    fetch(`http://94.103.87.164:8081/api/v1/${typeOfUser}-requests/${id}/reject`, {
         method: 'POST',
         headers: {
             'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
         }
     })
     .then ((response) => {
-        console.log(response)
         if (response.ok) {
-            deleteApplication(id)
-            return response.json()
-        } else {
-            
+            $('.account-status').removeClass('d-none')
+            location.reload()
         }
-    })
-    .then ((json) => {
-        console.log(json)
     })
 }
 export default rejectApplication
