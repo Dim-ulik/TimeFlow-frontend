9dd6d590e1c553e22ba6acb9d750c9ab9d71120d
Rapp Anna <anjarapp18@gmail.com>
2023-03-12 10:46:28 +0700

    refreshToken function

--- authorize/refreshToken.js
+++ authorize/refreshToken.js
@@ -1,33 +1,24 @@
 function refreshToken() {
-    let body = {
-        "refreshToken": localStorage.getItem('refreshToken')
-    } 
-    console.log(body)
     console.log(localStorage.getItem('refreshToken'))
     fetch('http://94.103.87.164:8081/api/v1/refresh-tokens', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
       },
-        body: body
+        body: {
+            "refreshToken": localStorage.getItem('refreshToken')
+        }
     })
-    .then(response => response.json())
-
-    // .then((response) => {
-    //     console.log(response)
-    //     return response.end()
-        
-    //     // if (response.ok) {
-    //     //     return response.json()
-    //     //     .then((json) => {
-    //     //         localStorage.setItem('accessToken', json.accessToken)
-    //     //         localStorage.setItem('refreshToken', json.refreshToken)
-    //     //     })
-    //     // }
-    // })
-    // .then((json) => {
-    //     console.log(json)
-    // })
+    .then((response) => {
+        //console.log(response.json())        
+        if (response.ok) {
+            return response.json()
+            .then((json) => {
+                localStorage.setItem('accessToken', json.accessToken)
+                localStorage.setItem('refreshToken', json.refreshToken)
+            })
+        }
+    })
     
 }
 
